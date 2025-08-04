8502f50e5547f35ba88753ccc8fdcdc9490af8ae
mehakkhan123 <mahekkhan284@gmail.com>
2025-08-04 16:25:29 +0500

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,57 @@
+"use client"
+import Link from 'next/link'
+import  React, { useState } from 'react'
+import { Menu } from 'lucide-react'
+import { 
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTrigger,
+  SheetClose,
+  SheetTitle,
+  SheetHeader
+  
+  
+} from "@/components/ui/sheet"
+
+export default function Header() {
+  const [open, setOpen] = useState(false)
+  return (
+    <div className="flex justify-center">
+        <header className='w-full max-w-[1536px] flex md:flex-row justify-between items-center px-6'>
+            <h1 className='text-green-600 text-2xl font-bold cursor-pointer'>Recipe Blog</h1>
+            <nav>
+            <Sheet open={open} onOpenChange={setOpen}>
+              <SheetTrigger asChild className='md:hidden py-3' aria-label='Open navigation menu'>
+                {open?(<SheetClose asChild><button type='button'><Menu/></button></SheetClose>):(<button type='button'><Menu/></button>)}
+              </SheetTrigger>
+              <SheetContent className='bg-white border-none'>
+                <SheetHeader>
+              <SheetTitle className="sr-only">Portfolio Navigation</SheetTitle>
+                            <SheetDescription className="sr-only">Main navigation menu</SheetDescription>
+                            <h1 className='text-green-600 text-lg font-bold cursor-pointer'>Recipe Blog</h1>
+                            </SheetHeader>
+
+                                <ul className='uppercase text-left px-3 '>
+                                  
+                    <li className='p-3' onClick={() => setOpen(false)}><Link href={"/"}>Home</Link></li>
+                    <li className='p-3' onClick={() => setOpen(false)}><Link href={"/about"}>About</Link></li>
+                    <li className='p-3' onClick={() => setOpen(false)}><Link href={"/recipes"}>Recipes</Link></li>
+                </ul>
+  
+              </SheetContent>
+            </Sheet>
+                <ul className='hidden md:flex flex-row gap-5 font-medium'>
+                    <li className='p-3 hover:text-green-600 text-lg transition-all duration-300 ease-in-out'><Link href={"/"}>Home</Link></li>
+                    <li className='p-3 hover:text-green-600 text-lg transition-all duration-300 ease-in-out'><Link href={"/about"}>About</Link></li>
+                    <li className='p-3 hover:text-green-600 text-lg transition-all duration-300 ease-in-out'><Link href={"/recipes"}>Recipes</Link></li>
+                </ul>
+                </nav>
+  
+    </header>
+    </div>
+  )
+}
+
+
+
---
+++ src/components/Hero.tsx
@@ -0,0 +1,25 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+
+
+export default function Hero() {
+  const router = useRouter()
+  return (
+    <div className='flex flex-col w-full items-center px-6 max-w-[1536px] justify-center'> {/* main container */}
+        <div className='w-full sm:h-[400px] md:h-[450px] lg:h-[550px] xl:h-[700px] flex justify-center items-center relative border border-black'> {/* image and text container */}
+          <Image src={"/images/hero/pizza.jpg"} alt='Delicious pizza' fill priority className='object-cover'/>
+          <div className='z-10 text-white bg-black/50 flex flex-col items-center justify-center text-center sm:h-[400px] md:h-[450px] lg:h-[550px] xl:h-[700px] w-full p-7 xl:p-9'> {/* text container */}
+            <h1 className='text-2xl font-bold sm:text-4xl md:text-5xl xl:text-7xl'>Discover your Next Favourite Meal</h1>
+            <p className='text-lg py-4 sm:text-xl md:text-2xl xl:text-5xl md:py-8'>Explore a world of flavours from quick weeknight dinners to gourmet delights.</p>
+             <button onClick={()=>{
+              router.push("/recipes")
+             }} className='bg-green-600 py-2 px-5 md:py-3 rounded-full text-2xl xl:text-5xl cursor-pointer hover:bg-green-500 transition duration-500 ease-in-out font-semibold'>Browse Recipes</button>
+          </div>
+    
+        </div>
+    </div>
+  )
+}
