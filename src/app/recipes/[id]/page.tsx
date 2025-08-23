2135755db9f539fd041dce45789ad6aae603c8d8
mehakkhan123 <mahekkhan284@gmail.com>
2025-08-23 17:05:08 +0500

    update comments

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -43,31 +43,31 @@ export default async function RecipeDetail({params}:RecipeDetailPageProps) {
       
       <div className='flex flex-col md:flex-row w-full items-center justify-center gap-8 mt-4'>
         <Image src={getItem.image} alt={getItem.title} width={300} height={300} className="rounded-lg shadow-lg" />
-        <div className='p-6 rounded-lg font-semibold flex flex-col justify-center gap-3 md:gap-7 shadow-md w-full max-w-[400px] h-fit md:h-[300px] text-sm sm:text-lg md:text-xl text-green-700'>
-          <div className='flex justify-between'>
+        <div className='p-6 rounded-lg font-semibold flex flex-col justify-center gap-3 md:gap-7 shadow-md w-full max-w-[400px] h-fit md:h-[300px] text-sm sm:text-lg md:text-xl text-green-700'> {/* recipe summary container */}
+          <div className='flex justify-between'>  {/* category */}
             <div className='flex items-center gap-2'><FaFolder /> <h3>Category</h3></div>
             <h3>{getItem.category}</h3>
           </div>
-          <div className='flex justify-between'>
+          <div className='flex justify-between'> {/* total time */}
             <div className='flex items-center gap-2'><FaRegHourglassHalf /> <h3>Total Time</h3></div>
             <h3>{getItem.totalTime}</h3>
           </div>
-          <div className='flex justify-between'>
+          <div className='flex justify-between'> {/* difficulty */}
             <div className='flex items-center gap-2'><PiChefHatFill /> <h3>Difficulty</h3></div>
             <h3>{getItem.difficulty}</h3>
           </div>
-          <div className='flex justify-between'>
+          <div className='flex justify-between'> {/* yields */}
             <div className='flex items-center gap-2'><GiForkKnifeSpoon /> <h3>Yields</h3></div>
             <h3>{getItem.servings}</h3>
           </div>
-          <div className='flex justify-between'>
+          <div className='flex justify-between'> {/* cuisine */}
             <div className='flex items-center gap-2'><FaGlobeAmericas /> <h3>Cuisine</h3></div>
             <h3>{getItem.cuisine}</h3>
           </div>
         </div>
       </div>
       
-      <div className='mt-7 px-6'>
+      <div className='mt-7 px-6'>  {/* ingredients container */}
         <h2 className='font-bold text-xl'>Ingredients</h2>
         <ul className="list-[square] text-amber-600 font-semibold text-md md:text-2xl">
           {getItem.ingredients.map((item, index) => (
@@ -76,7 +76,7 @@ export default async function RecipeDetail({params}:RecipeDetailPageProps) {
         </ul>
       </div>
       
-      <div className='mt-7 px-6'>
+      <div className='mt-7 px-6'> {/* instructions container */}
         <h2 className='font-bold text-xl'>Instructions</h2>
         <ol className="list-decimal text-md md:text-xl">
           {getItem.instructions.map((item, index) => (
