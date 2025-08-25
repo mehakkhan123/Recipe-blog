f1317901feb0b5c2ab93f2667b7122f56000fb15
mehakkhan123 <mahekkhan284@gmail.com>
2025-08-25 17:07:30 +0500

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center mt-4">
         <footer className='w-full max-w-[1536px] flex flex-col justify-between items-center px-6 py-4 font-semibold text-center bg-green-600 text-white'>
             <h3>&copy; 2025 Recipe Blog. All rights reserved.</h3>
-            <h3>Designed with <span className='text-red'>❤</span> by Mehak Khan</h3>
+            <h3>Designed with <span className='text-red-700'>❤</span> by Mehak Khan</h3>
     </footer>
     </div>
   )
