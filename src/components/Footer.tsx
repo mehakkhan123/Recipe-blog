9d644dc662c4e1db80a9b983228f6089f5f83bfe
mehakkhan123 <mahekkhan284@gmail.com>
2025-08-25 17:05:02 +0500

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className="flex justify-center">
         <footer className='w-full max-w-[1536px] flex flex-col justify-between items-center px-6 py-4 font-semibold text-center bg-green-600 text-white'>
             <h3>&copy; 2025 Recipe Blog. All rights reserved.</h3>
-            <h3>Designed with ❤ by Mehak Khan</h3>
+            <h3>Designed with <span className='text-red'>❤</span> by Mehak Khan</h3>
     </footer>
     </div>
   )
